f25ec72142b3fa02d8ec7be58aa314facda2cf81
LunarDiver <25Prozent@gmail.com>
2020-11-04 11:23:07 +0100

    Add project files.

---
+++ FlattenDirectory/Flattener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FlattenDirectory
+{
+	public static class Flattener
+	{
+		public static void Flatten(string dir)
+		{
+			string[] subfolders = Directory.GetDirectories(dir);
+			for(int i = 0; i < subfolders.Length; i++)
+			{
+				IEnumerable<string> files = Directory.EnumerateFiles(subfolders[i], "*.*", SearchOption.AllDirectories);
+
+				foreach(string file in files)
+				{
+					string filepath = file;
+
+					while(Path.GetDirectoryName(filepath) != Path.GetFullPath(dir))
+					{
+						string newPath = Path.GetDirectoryName(Path.GetDirectoryName(filepath));
+
+						if(newPath == null)
+							throw new DirectoryNotFoundException($"Could not find parent directory of file \"{filepath}\"");
+
+						newPath = Path.Combine(newPath, Path.GetFileName(filepath));
+
+						File.Move(filepath, newPath);
+						filepath = newPath;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ FlattenDirectory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FlattenDirectory
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine("Enter the full path to the directory you would like to flatten.");
+
+			askdir:
+			Console.Write("Directory: ");
+
+			string dir = Console.ReadLine();
+
+			if(!Directory.Exists(dir))
+			{
+				Console.WriteLine("This directory does not exist! Please try again.");
+				goto askdir;
+			}
+
+			bool noErrors = true;
+
+			Console.WriteLine("Flattening directory...");
+			try
+			{
+				Flattener.Flatten(dir);
+			}
+			catch(Exception exc)
+			{
+				noErrors = false;
+				Console.WriteLine($"Error: {exc.Message}");
+			}
+			Console.WriteLine($"Finished operation with {(noErrors ? "no" : "")} errors.");
+		}
+	}
+}
